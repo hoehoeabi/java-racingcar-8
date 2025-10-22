60a40dca58f5816bbed3bee33abd5ab2f66f20eb
hoehoeabi <0203jyh00@naver.com>
2025-10-22 16:44:05 +0900

    Feat(controller) :  컨트롤러 클래스
    - 검증을 컨트롤러에서 하는데 어디다 둬야 잘 뒀다고 소문이 날지 고민중입니다.

---
+++ src/main/java/racingcar/controller/RaceController.java
@@ -0,0 +1,43 @@
+package racingcar.controller;
+
+import racingcar.entity.Car;
+import racingcar.service.RaceService;
+import racingcar.validate.Validators;
+import racingcar.controller.view.RaceView;
+
+import java.util.List;
+import java.util.Queue;
+
+public class RaceController {
+
+    private final RaceView view;
+    private final Validators validators;
+    private final RaceService raceService;
+
+    public RaceController(RaceView view
+            ,Validators validators
+            ,RaceService raceService) {
+        this.view = view;
+        this.validators = validators;
+        this.raceService = raceService;
+    }
+
+    public void raceStart() {
+        String input = view.carsNameInputView();
+        validators.inputValidate(input);
+        raceService.createCars(input);
+
+        String raceReps = view.raceRepetitionInputView();
+        validators.repsValidate(raceReps);
+
+        view.outcomeTopView();
+
+        for(int i=0;i<Integer.parseInt(raceReps);i++){
+            List<Car> updatedCars = raceService.doOneRace();
+            view.raceFlowResultView(updatedCars);
+        }
+
+        Queue<String> winners = raceService.getWinners();
+        view.showWinnerOutputView(winners);
+    }
+}
