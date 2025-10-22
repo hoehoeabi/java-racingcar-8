14c0061489c1cadc29af10f86808762e5a415e76
hoehoeabi <0203jyh00@naver.com>
2025-10-22 16:18:50 +0900

    Feat(Car) : 자동차 객체를 만들었습니다
    - 생성자로는 오직 이름만 받을수 잇게 하였으며
    기본생성자와 이름과 진행상황을 받는 생성자는 선언을 막아두었습니다
    - 전진하는 메서드를 넣었습니다.
    - 우승자를 가릴때 필요하기에 이름과 진행상황을 가져오는 겟 메서드를 선언하였습니다

---
+++ src/main/java/racingcar/entity/Car.java
@@ -0,0 +1,36 @@
+package racingcar.entity;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+
+    private String name;
+    private int position;
+
+    public Car(String carName) {
+        this.name = carName;
+        this.position = 0;
+    }
+
+    private Car(String carName, int position) {
+        throw new RuntimeException("초기 위치는 초기화 할 수 없습니다!");
+    }
+
+    private Car(){
+        throw new RuntimeException("기본생성자는 생성 할 수 없습니다!");
+    }
+
+    public void move() {
+        if( Randoms.pickNumberInRange(0, 9)>=4){
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
