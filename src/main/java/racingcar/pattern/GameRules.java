f1255f523952fca7e589842f00e4afdf5a54ae9e
hoehoeabi <0203jyh00@naver.com>
2025-10-22 16:29:16 +0900

    Feat(validator) : 사용자 입력값을 검증할 상수들이 담긴 유틸클래스
    - 공통 검증: 이름은 다섯자리 이하여야하며 구분자는 ','만 올수 있습니다.
    - ONLY_ONE_CAR_REGEX : 자동차가 하나만입력되었는지에 대한 정규식 입니다.
    - RACING_CARS_REGEX : 이름사이에 공백도 허용가능하게 하였으며 최대 다섯개의 이름만 입력 가능하게 하였습니다.
    - MAX_RACE_REPETITION: 레이스횟수는 최대 5번만 가능하게 하였습니다

---
+++ src/main/java/racingcar/pattern/GameRules.java
@@ -0,0 +1,19 @@
+package racingcar.pattern;
+
+import java.util.regex.Pattern;
+
+public class GameRules {
+
+    private static final String ONLY_ONE_CAR_REGEX = "^([a-zA-Z]{1,5}){1}$";
+    private static final String RACING_CARS_REGEX = "^\\s*([a-zA-Z]{1,5})(\\s*,\\s*[a-zA-Z]{1,5}){0,4}\\s*$";
+
+    public static final Pattern ONLY_ONE_CAR_PATTERN = Pattern.compile(ONLY_ONE_CAR_REGEX);
+    public static final Pattern RACING_CARS_PATTERN = Pattern.compile(RACING_CARS_REGEX);
+
+    public static final Integer MAX_RACE_REPETITION = 5;
+
+    // 유틸리티 클래스이므로 인스턴스화 방지
+    private GameRules() {
+        throw new IllegalStateException("Rules class");
+    }
+}
