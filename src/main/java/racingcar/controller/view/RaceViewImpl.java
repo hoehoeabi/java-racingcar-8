b8198e2bd091167a73d79636540a7d0f338c4323
hoehoeabi <0203jyh00@naver.com>
2025-10-22 16:42:10 +0900

    Feat(view) : 뷰 클래스
    사용자 입력값을 입력받고 반환하는 뷰들은 InputView라고 붙여뒀습니다.
    컨트롤러에서 서비스를 사용해 차객체 리스트,우승자이름이 담긴 큐를 매개변수로 받게 하였습니다.
    
    - showWinnerOutputView에서 주석처리한 스트림은 제가 스트림을 아직 잘 모르기에 나중에 참고하려고 젬미니한테 부탁해 얻어온 코드입니다.

---
+++ src/main/java/racingcar/controller/view/RaceViewImpl.java
@@ -0,0 +1,55 @@
+package racingcar.controller.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.entity.Car;
+
+import java.util.List;
+import java.util.Queue;
+
+public class RaceViewImpl implements RaceView {
+
+    @Override
+    public String carsNameInputView(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+
+    @Override
+    public String raceRepetitionInputView(){
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return Console.readLine();
+    }
+
+    @Override
+    public void outcomeTopView() {
+        Console.close();
+        System.out.println("\n실행 결과");
+    }
+
+    @Override
+    public void raceFlowResultView(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.print(car.getName()+" : ");
+            for (int i = 0; i < car.getPosition(); i++) {
+                System.out.print("-");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    @Override
+    public void showWinnerOutputView(Queue<String> winners) {
+        System.out.print("최종 우승자 : "+ winners.poll());
+        if (!winners.isEmpty()) {
+            System.out.print(", " + winners.poll());
+        }
+//        String winnerList = winners.stream()
+//                .collect(Collectors.joining(", "));
+//
+//        System.out.println("최종 우승자 : " + winnerList);
+
+    }
+
+
+}
