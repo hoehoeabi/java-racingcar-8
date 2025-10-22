38687f00e2ff40663140034fc0028705938788ad
hoehoeabi <0203jyh00@naver.com>
2025-10-22 16:45:11 +0900

    Feat(main) :  앱의 시작점
    AppConfig로부터 의존성 주입이 끝난 컨트롤러의 raceStart 메서드를 호출합니다.
    
    실행과정중 생긴 모든 예외는 여기서 잡습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,20 @@
 package racingcar;
 
+import racingcar.config.AppConfig;
+import racingcar.controller.RaceController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        AppConfig appConfig = new AppConfig();
+
+        try{
+            appConfig
+                    .raceController()
+                    .raceStart();
+        } catch (Exception e){
+            System.out.println("[ERROR] : " + e.getMessage());
+        }
+
     }
 }
